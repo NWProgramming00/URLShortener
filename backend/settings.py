e321c43ec7600e82f00f569f85d51bc795be57fd
nwitt <nwitt@inpost.pl>
2024-02-07 17:21:58 +0100

    settings.py

---
+++ backend/settings.py
@@ -0,0 +1,18 @@
+from pydantic import (BaseModel)
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class DatabaseSettings(BaseModel):
+    host: str = 'test.db'
+    model_config = SettingsConfigDict(env_prefix='db_')
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix='app_')
+    host: str = ""
+    algorithm_salt: str = "SECURESALTFORENCODING"
+    algorithm_min_length: int = 6
+    database: DatabaseSettings = DatabaseSettings()
+
+
+settings = Settings()
